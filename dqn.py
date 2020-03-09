c5bb8423d35a30b111368aa4e683e8915a043f92
Rabona17 <y1gao@ucsd.edu>
2020-03-08 17:15:00 -0700

    add classes and utils

---
+++ dqn.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers,optimizers,losses
+
+class dqn(keras.Model):
+    def __init__(self):
+        super(dqn, self).__init__()
+        self.layer1 = layers.Dense(64, activation='relu', kernel_initializer='he_normal', )
+        self.layer2 = layers.Dense(64, activation='relu', kernel_initializer='he_normal')
+        self.layer3 = layers.Dense(2, kernel_initializer='he_normal')
+
+    def call(self, x, training=None):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        return out
+
