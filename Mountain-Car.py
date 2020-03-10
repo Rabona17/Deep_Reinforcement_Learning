a7d09c08855d80848244c34d960ea208edcbd481
Rabona17 <y1gao@ucsd.edu>
2020-03-10 14:24:59 -0700

    commit
    Co-Authored-By: Yuan Gao <y1gao@ucsd.edu>

--- Mountain-Car.py
+++ Mountain-Car.py
@@ -16,42 +16,44 @@ def main():
     optimizer = optimizers.Adam(lr=0.02)
     score_arr = np.array([])
     t_ = np.array([])
-    env = gym.make('CartPole-v1')
+    env = gym.make('MountainCar-v0')
     env.seed(1234)
     q = dqn(env.action_space.n)
     q_target = dqn(env.action_space.n)
-    q.build(input_shape=(None,4))
-    q_target.build(input_shape=(None,4))
+    q.build(input_shape=(None,env.observation_space.shape[0]))
+    q_target.build(input_shape=(None,env.observation_space.shape[0]))
     memory = replay_buffer()
     copy_round = 20
     score = 0.0
     for src, dest in zip(q.variables, q_target.variables):
         dest.assign(src)
-    for n_epi in range(1, 3101):
+    for n_epi in range(1, 3000):
         s = env.reset()
         for t in range(1200):
+            if(n_epi>1500):
+                env.render()
             a = epi_greedy(max(0.1, 0.4 - 0.01 * (n_epi / 200)), s,q,env.action_space.n)
+            #print(a)
             s_prime, r, done, info = env.step(a)
-            original_r = r
             #MODIFIED REWARD
-            x, x_dot, theta, theta_dot = s_prime
-            r1 = (env.x_threshold - abs(x))/env.x_threshold - 0.8
-            r2 = (env.theta_threshold_radians - abs(theta))/env.theta_threshold_radians - 0.5
-            r = r1 + r2
+            pos, vel = s_prime
+            r = 3*abs(pos+0.5)
             done_mask = int(not done)
-            memory.add_to_buffer(s, r / 100.0, a, s_prime, done_mask)
+            memory.add_to_buffer(s, r , a, s_prime, done_mask)
             s = s_prime
             score += r
             if done:
                 break
         if memory.size() > 5000:
             train(memory,q, q_target, 0.99,64,optimizer)
-        if n_epi % copy_round == 0:
+
+        if n_epi % 60 == 0:
             for src, dest in zip(q.variables, q_target.variables):
                 dest.assign(src)
-            print(n_epi, score/copy_round, original_r)
+        if n_epi % copy_round == 0:
+            print(n_epi, score/copy_round)
             t_ = np.append(t_, n_epi)
-            score_arr = np.append(score_arr, score/copy_round)
+            score_arr = np.append(score_arr, score/copy_round/3)
             score = 0.0
             
             
--- dqn.py
+++ dqn.py
@@ -3,11 +3,11 @@ from tensorflow import keras
 from tensorflow.keras import layers,optimizers,losses
 
 class dqn(keras.Model):
-    def __init__(self):
+    def __init__(self, n):
         super(dqn, self).__init__()
         self.layer1 = layers.Dense(64, activation='relu', kernel_initializer='he_normal', )
         self.layer2 = layers.Dense(64, activation='relu', kernel_initializer='he_normal')
-        self.layer3 = layers.Dense(2, kernel_initializer='he_normal')
+        self.layer3 = layers.Dense(n, kernel_initializer='he_normal')
 
     def call(self, x, training=None):
         out = self.layer1(x)
